1743ae527f84c90fddd612587df30863d8c22711
Schlanger <leoschlanger@gmail.com>
2020-04-16 08:36:05 -0300

    challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,14 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +22,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length === 0) {
+      return { income: 0, outcome: 0, total: 0 };
+    }
+
+    const balance: Balance = {
+      income: this.transactions.reduce((total, current) => {
+        return current.type === 'income' ? total + current.value : total;
+      }, 0),
+      outcome: this.transactions.reduce((total, current) => {
+        return current.type === 'outcome' ? total + current.value : total;
+      }, 0),
+      total: this.transactions.reduce((total, current) => {
+        return current.type === 'income'
+          ? total + current.value
+          : total - current.value;
+      }, 0),
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome' && this.getBalance().total - value < 0) {
+      throw Error('Invalid transaction.');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
